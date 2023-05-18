4e9bc05d4f6548ae5038cdf2df74532ca72daf2f
Wade Desco <descowade@gmail.com>
2023-05-17 17:01:10 -0700

    To change to main branch

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleSidebar() {
-    const sidebar = document.querySelector('.sidebar');
-    const content = document.querySelector('.content');
-  
-    sidebar.classList.toggle('collapsed');
-    content.classList.toggle('collapsed');
-  }
-  
